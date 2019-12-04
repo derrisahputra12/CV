b445588f37a2704011101c05152755dd5df427c5
derrisahputra12 <derrisahputra@gmail.com>
2019-12-04 08:11:21 +0700

    Ini Index

---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $(".btnHeart").mouseenter(function() {
+        $(this).html("Click <b>Now</b>")
+    
+    $(".btnHeart").mouseleave(function() {
+        $(this).html("Follow Me")
+    })
+    
+    $(".btnInstagram").mouseenter(function() {
+        $(this).html("derrisahputra")
+    })
+    $(".btnInstagram").mouseleave(function() {
+        $(this).html("My Instagram")
+    })
+   
+})})
